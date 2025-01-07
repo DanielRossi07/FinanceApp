eb1f289fe6d6c744721265bfcea845d2d5eeb52f
Daniel Rossi <danielrossimacedo07@gmail.com>
2025-01-06 22:27:52 -0300

    Feat - TransactionSource
    Add transaction source initialization

--- src/Finance.Application/UseCases/TransactionSource/Create/CreateTransactionSource.cs
+++ src/Finance.Application/UseCases/TransactionSource/Create/CreateTransactionSource.cs
@@ -2,6 +2,7 @@
 using Finance.Application.UseCases.TransactionSource.Common;
 using Finance.Domain.Repository;
 using DomainEntity = Finance.Domain.Entity;
+using SeedWork = Finance.Domain.SeedWork;
 
 namespace Finance.Application.UseCases.TransactionSource
 {
@@ -18,11 +19,41 @@ namespace Finance.Application.UseCases.TransactionSource
 
         public async Task<TransactionSourceModelOutput> Handle(CreateTransactionSourceInput input, CancellationToken cancellationToken)
         {
-            // TODO: Define how to initialize transactions: If else, strategy pattern, reflection
-            var transactionSource = new DomainEntity.Default(
-                input.Name,
-                input.BankAccountId,
-                input.UserId);
+            // TODO: Change initialization to: strategy pattern. Maybe add reflection to use the constructor instead of adding a Create method
+            SeedWork.TransactionSource transactionSource;
+            switch (input.Type)
+            {
+                case Domain.Enum.TransactionSourceType.Billet:
+                    transactionSource = new DomainEntity.Billet(
+                    input.Name,
+                    input.BankAccountId,
+                    input.UserId);
+                    break;
+                case Domain.Enum.TransactionSourceType.Card:
+                    transactionSource = new DomainEntity.Card(
+                    input.Name,
+                    input.BankAccountId,
+                    input.UserId);
+                    break;
+                case Domain.Enum.TransactionSourceType.Transfer:
+                    transactionSource = new DomainEntity.Transfer(
+                    input.Name,
+                    input.BankAccountId,
+                    input.UserId);
+                    break;
+                case Domain.Enum.TransactionSourceType.Pix:
+                    transactionSource = new DomainEntity.Pix(
+                    input.Name,
+                    input.BankAccountId,
+                    input.UserId);
+                    break;
+                default:
+                    transactionSource = new DomainEntity.Default(
+                    input.Name,
+                    input.BankAccountId,
+                    input.UserId);
+                    break;
+            }
 
             await _transactionSourceRepository.Insert(transactionSource, cancellationToken);
             await _unitOfWork.Commit(cancellationToken);
